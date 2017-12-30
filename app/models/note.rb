186d86af7eb42cdad3c8f6f5bd1b4d37083a4ee5
mkawasaki <mkawasaki@sbihg.co.jp>
2017-12-30 14:11:32 +0900

    run rubocop

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Note < ApplicationRecord
   belongs_to :project
   belongs_to :user
@@ -7,18 +9,18 @@ class Note < ApplicationRecord
   validates :message, presence: true
 
   scope :search, ->(term) {
-    where("LOWER(message) LIKE ?", "%#{term.downcase}%")
+    where('LOWER(message) LIKE ?', "%#{term.downcase}%")
   }
 
   has_attached_file :attachment
 
   validates_attachment :attachment,
-    content_type: {
-      content_type: [
-        "image/jpeg",
-        "image/gif",
-        "image/png",
-        "application/pdf",
-      ],
-    }
+                       content_type: {
+                         content_type: [
+                           'image/jpeg',
+                           'image/gif',
+                           'image/png',
+                           'application/pdf'
+                         ]
+                       }
 end
