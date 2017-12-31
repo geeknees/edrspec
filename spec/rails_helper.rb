a8633d6da626f39db5259e9aac937f504237eb9c
mkawasaki <mkawasaki@sbihg.co.jp>
2018-01-01 00:55:17 +0900

    add request specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,5 @@ RSpec.configure do |config|
 
   # Use Devise helpers in tests
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include RequestSpecHelper, type: :request
 end
